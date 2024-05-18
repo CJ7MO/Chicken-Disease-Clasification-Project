7dcc921be0df927039c175bc1365c71d04e6c52c
cj7mo <jcobando@uninorte.edu.co>
2024-05-18 13:15:10 -0500

    prepare base model added

--- src/cnnClassifier/components/prepare_base_model.py
+++ src/cnnClassifier/components/prepare_base_model.py
@@ -58,6 +58,8 @@ class PrepareBaseModel:
             learning_rate=self.config.params_learning_rate
         )
 
+        self.save_model(path=self.config.updated_base_model_path, model=self.full_model)
+
     @staticmethod
     def save_model(path: Path, model: tf.keras.Model):
         model.save(path)
